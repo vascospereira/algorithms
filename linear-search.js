5be9f03d1d17849bcfe9f03410978610f340c6b3
vascospereira <vascospereira@outlook.com>
2018-01-18 22:59:15 +0000

    sort and search

---
+++ linear-search.js
@@ -0,0 +1,13 @@
+var doLinearSearch = function(array) {
+  for (var guess = 0; guess < array.length; guess++) {
+    if (array[guess] === targetValue) { 
+        return guess;
+    }
+  }
+  return -1;
+};
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+    41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = doLinearSearch(primes, 79);
