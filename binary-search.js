1db22e600b510ff806b09d021fa382d31b827a6b
vascospereira <vascospereira@outlook.com>
2018-01-19 15:28:19 +0000

    sort and search

--- binary-search.js
+++ binary-search.js
@@ -1,6 +1,6 @@
 /* Returns either the index of the location in the array,
   or -1 if the array did not contain the targetValue */
-var doSearch = function(array, targetValue) {
+var doBinarySearch = function(array, targetValue) {
 	var min = 0;
 	var max = array.length - 1;
     var guess;
@@ -21,4 +21,4 @@ var doSearch = function(array, targetValue) {
 var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
 		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
-var result = doSearch(primes, 73);
+var result = doBinarySearch(primes, 73);
