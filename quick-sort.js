4806f848c3340dc7174debf7aab4b32440039df2
vascospereira <vascospereira@outlook.com>
2018-01-21 20:23:49 +0000

    sort and search

--- quick-sort.js
+++ quick-sort.js
@@ -1,3 +1,21 @@
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+var partition = function(array, p, r) {
+    var i = p;
+    for(var j = p; j < r; j++){
+        if(array[j] <= array[r]){
+            swap(array, j, i);
+            i++;
+        }
+    }
+    swap(array, j, i);
+    return i;
+};
+
 var quickSort = function(array, p, r) {
     if(p < r){
         var q = partition(array, p, r);
@@ -5,6 +23,9 @@ var quickSort = function(array, p, r) {
         quickSort(array, q + 1, r);
     }
 };
+
+var array = [9, 7, -5, 11, 12, -2, 14, 0, 10, 7];
+quickSort(array, 0, array.length - 1);
 
 var array = [9, 7, 5, 11, 12, 2, 14, 3, 10, 6];
 quickSort(array, 0, array.length - 1);
