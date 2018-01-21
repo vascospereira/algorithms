ba2e5ad13caaed0d24619d5cf053f1050c995b5f
vascospereira <vascospereira@outlook.com>
2018-01-21 13:09:48 +0000

    sort and search

---
+++ quick-sort.js
@@ -0,0 +1,10 @@
+var quickSort = function(array, p, r) {
+    if(p < r){
+        var q = partition(array, p, r);
+        quickSort(array, p, q - 1);
+        quickSort(array, q + 1, r);
+    }
+};
+
+var array = [9, 7, 5, 11, 12, 2, 14, 3, 10, 6];
+quickSort(array, 0, array.length - 1);
